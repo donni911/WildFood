7071f79848f4f1bdd3f6b334ab35b0336b6e1d46
donni911 <rodion2001vo@gmail.com>
2023-06-19 16:32:24 +0300

    fix banner && add carousel

--- resources/js@src/modules/gallery/index.js
+++ resources/js@src/modules/gallery/index.js
@@ -1,16 +1,18 @@
-import PhotoSwipe from 'photoswipe';
-import PhotoSwipeLightbox from 'photoswipe/lightbox';
+// import PhotoSwipe from 'photoswipe';
+// import PhotoSwipeLightbox from 'photoswipe/lightbox';
 
-const galleryEls = document.querySelectorAll('[data-gallery]');
+// const galleryEls = document.querySelectorAll('[data-gallery]');
 
-galleryEls.forEach(galleryEl => {
-    const childrenEls = galleryEl.querySelectorAll('[data-gallery-link]');
 
-    const lightbox = new PhotoSwipeLightbox({
-        gallery: galleryEl,
-        children: childrenEls,
-        showHideAnimationType: 'fade',
-        pswpModule: PhotoSwipe,
-    });
-    lightbox.init();
-});
+// galleryEls.forEach(galleryEl => {
+//     const childrenEls = galleryEl.querySelectorAll('[data-gallery-link]');
+    
+//     const lightbox = new PhotoSwipeLightbox({
+//         gallery: galleryEl,
+//         children: childrenEls,
+//         showHideAnimationType: 'fade',
+//         pswpModule: PhotoSwipe,
+//     });
+//     console.log(lightbox);
+//     lightbox.init();
+// });
