9fc230aef4068abb5d404c590863510e5dfbc7ca
donni911 <rodion2001vo@gmail.com>
2023-06-21 11:18:34 +0300

    add tabs

---
+++ static/build/js/6.14847ebe.js
@@ -0,0 +1,2 @@
+import"./chunks/virtual_svg-icons-register.767c5017.js";const s=Object.assign({});Object.keys(s).forEach(c=>s[c]());
+//# sourceMappingURL=6.14847ebe.js.map
--- static/build/js/6.41e57a55.js
+++
@@ -1,2 +0,0 @@
-import"./chunks/virtual_svg-icons-register.ca8dec66.js";const s=Object.assign({});Object.keys(s).forEach(c=>s[c]());
-//# sourceMappingURL=6.41e57a55.js.map
